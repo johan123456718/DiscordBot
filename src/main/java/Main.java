b846d0698db1e9a7732cafb821b826535e223a4a
johan123456718 <axel1234516@hotmail.com>
2020-07-18 17:00:34 +0200

    add JDA builder

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,13 @@
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+import javax.security.auth.login.LoginException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("hello");
+    public static void main(String[] args) throws LoginException, InterruptedException {
+        JDA jda = JDABuilder.createDefault("token")
+                .build();
+
+        jda.awaitReady();
     }
 }
